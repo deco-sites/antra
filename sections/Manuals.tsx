db2bd1a4a872e58a710a2bba62b860ec67d810dd
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-09-25 17:38:55 -0300

    [WIP]

--- sections/Manuals.tsx
+++ sections/Manuals.tsx
@@ -1,29 +1,49 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Icon from "site/components/ui/Icon.tsx";
+import Slider from "site/components/ui/Slider.tsx";
 
 interface Card {
-  title: string
-  image: ImageWidget
-  tag: string[]
+  title: string;
+  image: ImageWidget;
+  tag: string[];
 }
 
 interface Props {
-  cards: Card[]
+  cards: Card[];
 }
 
-const DEFAULT_IMAGE =
-  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4763/682eb374-def2-4e85-a45d-b3a7ff8a31a9";
-
-export default function Manuals({
-  cards
-}: Props) {
+export default function Manuals({ cards }: Props) {
   return (
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 text-sm py-8 lg:py-20">
       <div>
         <h1>Cartilhas e Manuais</h1>
       </div>
-      
+
+      <Slider rootId="manuals-slider" scroll="smooth" interval={5000} infinite>
+        {cards.map((card, index) => (
+          <Slider.Item key={card.title} index={index} class="flex justify-center items-center">
+            <Image
+              src={card.image}
+              alt={card.image}
+              width={300}
+              height={200}
+              class="object-cover"
+              decoding="async"
+              loading="lazy"
+            />
+            <div class="p-4">
+              <h3 class="text-lg font-bold">{card.title}</h3>
+              {/* Exibindo tags */}
+              <div class="text-gray-500">
+                {card.tag.map((tag) => (
+                  <span key={tag} class="mr-2">{tag}</span>
+                ))}
+              </div>
+            </div>
+          </Slider.Item>
+        ))}
+      </Slider>
     </div>
   );
 }
