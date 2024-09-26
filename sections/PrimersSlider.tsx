66d3a43048f8466beeceb086ff54a080f45da5f7
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-09-26 12:17:37 -0300

    [WIP]

--- sections/PrimersSlider.tsx
+++ sections/PrimersSlider.tsx
@@ -3,7 +3,7 @@ import { h, FunctionalComponent } from "preact";
 import { clx } from "site/sdk/clx.ts";
 import { useId } from "site/sdk/useId.ts";
 import Slider from "site/components/ui/Slider.tsx";
-import SliderJS from "apps/utils/components/SliderJS.tsx";
+import Icon from "site/components/ui/Icon.tsx";
 
 interface SlideProps {
   image: ImageWidget;
@@ -15,56 +15,42 @@ interface SliderProps {
   slides: SlideProps[];
 }
 
-const Slide: FunctionalComponent<SlideProps> = ({
-  image,
-  title,
-  tag,
-}: SlideProps) => {
-  return (
-    <div class="slide">
-      <img src={image} alt={title} />
-      <h2 class="slide-title">{title}</h2>
-      <p class="slide-description">{tag}</p>
-    </div>
-  );
-};
 
-const PrimersSlider: FunctionalComponent<SliderProps> = ({
-  slides,
-}: SliderProps) => {
-  const id = useId();
+function Card({ image, title, tag }: SlideProps) {
   return (
-    <>
-    <div
-      id={id}
-      class="grid grid-rows-1"
-      style={{
-        gridTemplateColumns: "min-content 1fr min-content",
-      }}
-    >
-      <div class="col-start-1 col-span-3 row-start-1 row-span-1">
-        <Slider
-          rootId={id}
-          class="carousel carousel-center sm:carousel-end gap-5 sm:gap-10 w-full"
-        >
-          {slides.map((slide, index) => (
-            <Slider.Item
-              index={index}
-              class={clx(
-                "carousel-item",
-                "first:pl-5 first:sm:pl-0",
-                "last:pr-5 last:sm:pr-0"
-              )}
-            >
-              {slide}
-            </Slider.Item>
-          ))}
-        </Slider>
-      </div>{" "}
+    <div class="p-4">
+      <img src={image} alt={image} class="w-full h-48 object-cover rounded-md" />
+      <h3 class="mt-2 text-lg font-semibold">{title}</h3>
+      <div class="mt-2">
+        {tag.map((t) => (
+          <span class="text-sm bg-gray-200 rounded-full px-2 py-1 mr-2">{t}</span>
+        ))}
+      </div>
     </div>
-    <SliderJS rootId={id} />
-    </>
   );
-};
+}
+
+function PrimersSlider({ slides }: SliderProps) {
+  const id = useId()
+  return (
+    <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 text-sm py-8 lg:py-20 border-2 border-gray-700">
+      <div class="space-y-10">
+    <Slider rootId="card-slider">
+      {slides.map((slide, index) => (
+        <Slider.Item key={index} index={index}>
+          <Card image={slide.image} title={slide.title} tag={slide.tag} />
+        </Slider.Item>
+      ))}
+      <Slider.PrevButton class="transform -translate-y-1/2 p-2 bg-gray-300 rounded-full">
+      <Icon id="ChevronRight" size={24} strokeWidth={2} />
+      </Slider.PrevButton>
+      <Slider.NextButton class="transform -translate-y-1/2 p-2 bg-gray-300 rounded-full">
+      <Icon id="ChevronLeft" size={24} strokeWidth={2} />
+      </Slider.NextButton>
+    </Slider>
+      </div>
+    </div>
+  );
+}
 
 export default PrimersSlider;
