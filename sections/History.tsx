524311918a8320e352b82071996b3819027530ab
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-10-03 11:21:19 -0300

    [WIP]

--- sections/History.tsx
+++ sections/History.tsx
@@ -1,10 +1,50 @@
-
-
-export default function History() {
+import Image from "apps/website/components/Image.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
+import { Text1 } from "site/islands/text1-history.tsx";
+import { Text2 } from "site/islands/text2-history.tsx";
+interface Props {
+  image1: ImageWidget;
+  image2: ImageWidget;
+}
+export default function History({ image1, image2 }: Props) {
   return (
-    <div className="lg:container text-sm mb-10">
-      <div className="space-y-10 px-6 md:px-24 py-8 md:py-24">
-
+    <div className="lg:container text-sm mb-40">
+      <div className="space-y-10">
+        <div class="flex flex-col items-center gap-8">
+          <div class="w-full max-w-[850px] flex flex-col">
+            <h2 class="text-6xl font-bold text-black leading-snug">
+              E assim nasceu o
+            </h2>
+            <h2 class="text-6xl font-bold text-black leading-snug text-center">
+              <span class="text-blue-400">movimento</span>{" "}
+              <span class="text-pink-500">nacional</span>
+            </h2>
+            <h2 class="text-6xl font-bold text-black leading-snug text-right">
+              de <span class="text-black">Travestis e Transexuais</span>.
+            </h2>
+            <p class="text-gray-500 mt-2 text-right">Keila Simpson Sousa</p>
+          </div>
+          <Text1 />
+          <Text2 />
+          <div class="flex max-w-[800px]">
+            <Image
+              className="w-full rounded-2xl"
+              width={380}
+              src={image1}
+              alt={image1}
+              decoding="async"
+              loading="lazy"
+            />
+            <Image
+              className="w-full rounded-2xl"
+              width={380}
+              src={image2}
+              alt={image2}
+              decoding="async"
+              loading="lazy"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
